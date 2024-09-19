7d08bfed361492ce22f6201de5b8065c6b2e6de8
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2024-09-19 19:47:57 +0700

    update searchable column in fk table

--- app/DataTables/OpportunityStateDataTable.php
+++ app/DataTables/OpportunityStateDataTable.php
@@ -61,7 +61,38 @@ class OpportunityStateDataTable extends DataTable
             ->setTableId('dataTable')
             ->columns($this->getColumns())
             ->minifiedAjax()
-            ->dom('<"row align-items-center"<"col-md-2" l><"col-md-6" B><"col-md-4"f>><"table-responsive my-3" rt><"row align-items-center" <"col-md-6" i><"col-md-6" p>><"clear">')
+            ->dom('<"row align-items-center"<"col-md-6" l><"col-md-6"f>><"row my-3"<"col-md-12"B>><"table-responsive" rt><"row align-items-center" <"col-md-6" i><"col-md-6" p>><"clear">')
+            ->buttons(
+                [
+                    [
+                        'text' => 'Reset',
+                        'className' => 'btn btn-secondary',
+                        'action' => 'function(e, dt, button, config) {
+                            dt.columns(3).search("^((?!4|5).)*$", true, false).draw(); // Show all except Completed and Failed
+                        }'
+                    ],
+                    [
+                        'text' => 'Completed',
+                        'className' => 'btn btn-success',
+                        'action' => 'function(e, dt, button, config) {
+                            dt.columns(3).search("^4$", true, false).draw(); // Show only Completed
+                        }'
+                    ],
+                    [
+                        'text' => 'Failed',
+                        'className' => 'btn btn-danger',
+                        'action' => 'function(e, dt, button, config) {
+                            dt.columns(3).search("^5$", true, false).draw(); // Show only Failed
+                        }'
+                    ],
+                    // [
+                    //     'extend' => 'collection',
+                    //     'text' => 'Created By',
+                    //     'className' => 'btn btn-info dropdown-toggle',
+                    //     'buttons' => $this->getCreatedByDropdownOptions() // Fetch dropdown options here
+                    // ]
+                ]
+            )
             ->parameters([
                 "processing" => true,
                 "autoWidth" => false,
@@ -101,7 +132,7 @@ class OpportunityStateDataTable extends DataTable
 
     protected function getCreatedByDropdownOptions()
     {
-        $createdByUsers = \App\Models\User::select('id', 'name')->get();
+        $createdByUsers = User::select('id', 'name')->get();
 
         $dropdownOptions = [];
 
