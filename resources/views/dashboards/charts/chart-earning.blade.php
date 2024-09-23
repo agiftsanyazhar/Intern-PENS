a64fd0086eedf0adf550e4f6e8b1c22c2c18f715
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2024-09-23 21:03:15 +0700

    update chart

--- resources/views/dashboards/charts/chart-earning.blade.php
+++ resources/views/dashboards/charts/chart-earning.blade.php
@@ -2,19 +2,23 @@
     <div class="card" data-aos="fade-up" data-aos-delay="800">
         <div class="card-header d-flex justify-content-between flex-wrap">
             <div class="header-title">
-            <h4 class="card-title">$855.8K</h4>
+                <h4 class="card-title">Total: </h4>
+                <h6 class="card-title text-success completed">Completed: </h6>
+                <h6 class="card-title text-danger failed">Failed: </h6>
             </div>
             <div class="dropdown">
-            <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                Last 24 Hours
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
-                <li><a class="dropdown-item" href="#">Last 24 Hours</a></li>
-                <li><a class="dropdown-item" href="#">Last 7 Days</a></li>
-                <li><a class="dropdown-item" href="#">Last 30 Days</a></li>
-                <li><a class="dropdown-item" href="#">Last 60 Days</a></li>
-                <li><a class="dropdown-item" href="#">Last 90 Days</a></li>
-                <li><a class="dropdown-item" id="custom-filter" href="#">Custom</a></li>
+                <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    Last 24 Hours
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">Last 24 Hours</a></li>
+                    <li><a class="dropdown-item" href="#">Last 7 Days</a></li>
+                    <li><a class="dropdown-item" href="#">Last 30 Days</a></li>
+                    <li><a class="dropdown-item" href="#">Last 60 Days</a></li>
+                    <li><a class="dropdown-item" href="#">Last 90 Days</a></li>
+                    <li><a class="dropdown-item" href="#">All Time</a></li>
+                    <li><a class="dropdown-item" id="custom-filter" href="#">Custom</a></li>
+                </ul>
             </div>
             <!-- Custom date range picker (hidden by default) -->
             <div id="custom-date-range" style="display: none; margin-top: 10px;">
--- resources/views/dashboards/dashboard.blade.php
+++ resources/views/dashboards/dashboard.blade.php
@@ -44,8 +44,12 @@
                      Rp                      
                   </div>
                   <div class="text-end">
-                     <h2 class="counter">Rp{{ formatCurrency($opportunityValue) }}</h2>
+                     <h2 class="counter">Rp{{ formatCurrency($opportunityGrossValue) }}</h2>
                      Total Earning
+                     {{-- <h2 class="counter">Rp{{ formatCurrency($opportunityCompletedValue) }}</h2>
+                     Completed Earning
+                     <h2 class="counter">Rp{{ formatCurrency($opportunityFailedValue) }}</h2>
+                     Failed Earning --}}
                   </div>
                </div>
             </div>
@@ -81,7 +85,7 @@
                   </div>
                   <div class="text-end">
                      <h2 class="counter">{{ formatCurrency($opportunityCompleted) }}</h2>
-                     Complete
+                     Completed
                   </div>
                </div>
             </div>
@@ -182,54 +186,6 @@
       <div class="col-md-12 col-lg-8">
          <div class="row">
             @include('dashboards.charts.chart-earning')
-            {{-- <div class="col-md-12 col-lg-6">
-               <div class="card" data-aos="fade-up" data-aos-delay="1000">
-                  <div class="card-header d-flex justify-content-between flex-wrap">
-                     <div class="header-title">
-                        <h4 class="card-title">Earnings</h4>
-                     </div>
-                     <div class="dropdown">
-                        <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                           This Week
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                           <li><a class="dropdown-item" href="#">This Week</a></li>
-                           <li><a class="dropdown-item" href="#">This Month</a></li>
-                           <li><a class="dropdown-item" href="#">This Year</a></li>
-                        </ul>
-                     </div>
-                  </div>
-                  <div class="card-body">
-                     <div class="d-flex align-items-center justify-content-between">
-                        <div id="myChart" class="col-md-8 col-lg-8 myChart"></div>
-                        <div class="d-grid gap col-md-4 col-lg-4">
-                           <div class="d-flex align-items-start">
-                              <svg class="mt-2" xmlns="http://www.w3.org/2000/svg" width="14" viewBox="0 0 24 24" fill="#3a57e8">
-                                 <g id="Solid dot">
-                                    <circle id="Ellipse 67" cx="12" cy="12" r="8" fill="#3a57e8"></circle>
-                                 </g>
-                              </svg>
-                              <div class="ms-3">
-                                 <span class="text-gray">Fashion</span>
-                                 <h6>251K</h6>
-                              </div>
-                           </div>
-                           <div class="d-flex align-items-start">
-                              <svg class="mt-2" xmlns="http://www.w3.org/2000/svg" width="14" viewBox="0 0 24 24" fill="#4bc7d2">
-                                 <g id="Solid dot1">
-                                    <circle id="Ellipse 68" cx="12" cy="12" r="8" fill="#4bc7d2"></circle>
-                                 </g>
-                              </svg>
-                              <div class="ms-3">
-                                 <span class="text-gray">Accessories</span>
-                                 <h6>176K</h6>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
             <div class="col-md-12 col-lg-6">
                <div class="card" data-aos="fade-up" data-aos-delay="1200">
                   <div class="card-header d-flex justify-content-between flex-wrap">
@@ -432,7 +388,7 @@
                      </div>
                   </div>
                </div>
-            </div> --}}
+            </div>
          </div>
       </div>
       {{-- <div class="col-md-12 col-lg-4">
